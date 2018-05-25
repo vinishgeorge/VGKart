f118cb83076af93a64d3a10cdb1a40fb675952d3
vinish.george1@gmail.com <vinish.george1@gmail.com>
2018-05-25 12:59:08 +0530

    working initial reactive form

--- src/app/signup-reactive/signup-reactive.component.ts
+++ src/app/signup-reactive/signup-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms'
 @Component({
   selector: 'app-signup-reactive',
   templateUrl: './signup-reactive.component.html',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupReactiveComponent implements OnInit {
 
+  private signupform: FormGroup;
+  private gender:Array<Object>=new Array();
   constructor() { }
 
   ngOnInit() {
+    var genderObj=new Object();
+    genderObj["value"]="M";
+    genderObj["label"]="Male";
+    this.gender.push(genderObj);
+    var genderObj=new Object();
+    genderObj["value"]="F";
+    genderObj["label"]="Female";
+    this.gender.push(genderObj);
+    var genderObj=new Object();
+    genderObj["value"]="T";
+    genderObj["label"]="Transgender";
+    this.gender.push(genderObj);
+    this.signupform = new FormGroup(
+      {
+        userName: new FormControl(''),
+        password: new FormControl(''),
+        confirmPassword:new FormControl(''),
+        gender: new FormControl(''),
+        terms: new FormControl(''),
+        email: new FormControl('')
+      }
+
+    );
   }
 
 }
