301225549f829693fa6495c87f09ee7d5c05f188
vinish.george1@gmail.com <vinish.george1@gmail.com>
2018-05-26 10:01:51 +0530

    validation working example  for reactive

--- src/app/signup-reactive/signup-reactive.component.ts
+++ src/app/signup-reactive/signup-reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { User } from '../signup-component/User';
 @Component({
   selector: 'app-signup-reactive',
   templateUrl: './signup-reactive.component.html',
@@ -8,33 +9,38 @@ import { FormGroup, FormControl } from '@angular/forms'
 export class SignupReactiveComponent implements OnInit {
 
   private signupform: FormGroup;
-  private gender:Array<Object>=new Array();
+  private gender: Array<Object> = new Array();
+  private user: Array<User> = new Array<User>();
   constructor() { }
 
   ngOnInit() {
-    var genderObj=new Object();
-    genderObj["value"]="M";
-    genderObj["label"]="Male";
+    var genderObj = new Object();
+    genderObj["value"] = "M";
+    genderObj["label"] = "Male";
     this.gender.push(genderObj);
-    var genderObj=new Object();
-    genderObj["value"]="F";
-    genderObj["label"]="Female";
+    var genderObj = new Object();
+    genderObj["value"] = "F";
+    genderObj["label"] = "Female";
     this.gender.push(genderObj);
-    var genderObj=new Object();
-    genderObj["value"]="T";
-    genderObj["label"]="Transgender";
+    var genderObj = new Object();
+    genderObj["value"] = "T";
+    genderObj["label"] = "Transgender";
     this.gender.push(genderObj);
     this.signupform = new FormGroup(
       {
-        userName: new FormControl(''),
-        password: new FormControl(''),
-        confirmPassword:new FormControl(''),
-        gender: new FormControl(''),
-        terms: new FormControl(''),
-        email: new FormControl('')
+        userName: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required,  Validators.minLength(6)]),
+        confirmPassword: new FormControl('', [Validators.required,  Validators.minLength(6)]),
+        gender: new FormControl('', Validators.required),
+        terms: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email, Validators.required])
       }
 
     );
   }
 
+  registerUser() {
+    if (this.signupform.valid)
+      this.user.push(this.signupform.value);
+  }
 }
